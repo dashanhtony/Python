24385bbec7c17c590fd2750213f710596fc876f3
shaoyuning@bestpay.com.cn <shaoyuning@bestpay.com.cn>
2017-03-03 18:06:26 +0800

    add

--- jenkins/PythonTest.py
+++ jenkins/PythonTest.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#100
-i = ['a', 'b']
-l = [1, 2]
-m = {}
-ri = range(len(i))
-rl = range(len(l))
 
-for c in ri:
-   for d in rl:
-       if c==d:
-           m[i[c]]=l[d]
-print (m)
 
-#it's ok now
+
+class dummyclass(object):
+    def __init__(self):
+        self.is_d = True
+        pass
+
+class childdummyclass(dummyclass):
+    def __init__(self, isman):
+        self.isman = isman
+
+    @classmethod
+    def can_speak(self): return True
+
+    @property
+    def man(self): return self.isman
+
+if __name__ == "__main__":
+    object1 = dummyclass()
+    object = childdummyclass('true')
+    print(object.can_speak())
+
+    print(object.is_d)
