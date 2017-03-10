d2d4ecf42432288af3c345023dea92c7272cf901
shaoyuning@bestpay.com.cn <shaoyuning@bestpay.com.cn>
2017-03-10 08:54:34 +0800

    add

--- jenkins/PythonTimeLearning.py
+++ jenkins/PythonTimeLearning.py
@@ -12,13 +12,13 @@ print("本地时间为 :", localtime)#本地时间为 : time.struct_time(tm_year
 localtime = time.asctime(time.localtime(time.time()))
 print("本地时间为 :", localtime)#本地时间为 : Mon Mar  6 08:58:45 2017
 
-# 格式化成2016-03-20 11:45:39形式
-print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))# 格式化成2016-03-20 11:45:39形式
 
-# 格式化成Sat Mar 28 22:24:24 2016形式
-print(time.strftime("%a %b %d %H:%M:%S %Y", time.localtime()))
+print(time.strftime("%a %b %d %H:%M:%S %Y", time.localtime()))# 格式化成Sat Mar 28 22:24:24 2016形式
 
-# 将格式字符串转换为时间戳
 a = "Sat Mar 28 22:24:24 2016"
+print("next strptime:")
 print(time.strptime(a,"%a %b %d %H:%M:%S %Y"))
-print(time.mktime(time.strptime(a,"%a %b %d %H:%M:%S %Y")))
+print("next mktime:")
+print(time.mktime(time.strptime(a,"%a %b %d %H:%M:%S %Y")))# 将格式字符串转换为时间戳
+
