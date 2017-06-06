51d5d6e64130ab2aee26c9faaa46fc47e321669b
shaoyuning@bestpay.com.cn <shaoyuning@bestpay.com.cn>
2017-06-06 14:43:34 +0800

    add awesome

--- jenkins/async/async_hello2.py
+++ jenkins/async/async_hello2.py
@@ -4,12 +4,14 @@
 import threading
 import asyncio
 
-async def hello():
+async def hello(x):
+    print(x)
     print('Hello world! (%s)' % threading.currentThread())
     await asyncio.sleep(3)
+    print(x)
     print('Hello again! (%s)' % threading.currentThread())
 
 loop = asyncio.get_event_loop()
-tasks = [hello(), hello()]
+tasks = [hello(2), hello(3)]
 loop.run_until_complete(asyncio.wait(tasks))
 loop.close()
