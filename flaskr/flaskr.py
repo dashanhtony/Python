7d52c15be54c5ba27cb76178c5595c76a140a474
shaoyuning@bestpay.com.cn <shaoyuning@bestpay.com.cn>
2017-06-07 11:17:06 +0800

    add templates files

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -19,6 +19,48 @@ def init_db():
         cursor.execute(f.read())
         conn.commit()
 
+@app.route('/index')
+def indexpage():
+    return '<b>This is indexpage /</b>'
+
+@app.route('/')
+def show_entries():
+    cur = cursor.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    cursor.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    conn.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
 init_db()
 if __name__ == '__main__':
-    app.run(port=8888)
+    app.debug = True
+    app.run(port=18888)
